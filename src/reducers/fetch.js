165bfe8138a61255f1df0207d619e4729ab29bab
Ivan Sugerman <ivan@getambassador.com>
2016-10-03 20:51:43 -0400

    added fetch reducer

---
+++ src/reducers/fetch.js
@@ -0,0 +1,18 @@
+import Immutable from 'immutable'
+import normalize from '../utils/normalize'
+
+const success = (state, action) => {
+
+  // If the returned data is a list we normalize and add each item
+  if(action.data && action.data instanceof Array) {
+    const normalizedData = normalize(action.data)
+    return state.merge('raw', normalizedData)
+  }
+
+  // Otherwise we update the single object
+  return state.setIn(['raw', action.uid, action.data])
+}
+
+export default {
+  success
+}
