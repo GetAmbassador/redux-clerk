19c8d2ae12eb024cc0400c29d1c3189a754fa100
Ivan Sugerman <ivan@getambassador.com>
2016-10-11 06:58:07 -0400

    added tests for the primary actions generator

--- src/actions.js
+++ src/actions.js
@@ -1,14 +1,25 @@
-import { Create, Insert, Update, Delete } from './actions'
+import { Create } from './actions/Create'
+import { Update } from './actions/Update'
+import { Delete } from './actions/Delete'
 
+/**
+ * Creates a new instance of each CRUD action
+ * @param  {Object} config - action configuration
+ *
+ * @return {Object} - the set of CRUD action creators
+ */
 const actions = (config) => {
+
+  if (!config) throw new Error('clerk.actions: Expected config')
+  if (!config.actionPrefix) throw new Error('clerk.actions: Expected actionPrefix')
+  if (!config.uidField) throw new Error('clerk.actions: Expected uidField')
+
   const createAction = new Create(config)
-  const insertAction = new Insert(config)
   const updateAction = new Update(config)
   const deleteAction = new Delete(config)
 
   return {
     create: createAction.do,
-    insert: insertAction.do,
     update: updateAction.do,
     delete: deleteAction.do
   }
