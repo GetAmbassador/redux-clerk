a00cff51725e8723d945569074063540eb9437c5
Ivan Sugerman <ivan@getambassador.com>
2016-10-29 18:53:15 -0400

    handle success in the create reducer

--- src/reducers/create.js
+++ src/reducers/create.js
@@ -9,6 +9,32 @@ export const start = (state, action) => {
   return state.setIn(['raw', action.data.get(action.uidField)], action.data)
 }
 
+/**
+ * The success action for the create reducer
+ * @param  {Immutable.Map} state - reducer configuration
+ *
+ * @return {Immutable.Map} - updated state
+ */
+export const success = (state) => {
+  // Currently we do nothing on success because
+  // the record is optimisticly updated in the success action.
+  // Eventually we'll set a loading state to false
+  return state
+}
+
+/**
+ * The error action for the create reducer
+ * @param  {Immutable.Map} state - reducer configuration
+ *
+ * @return {Immutable.Map} - updated state
+ */
+export const error = (state, action) => {
+  // Remove the added record on error because the request failed
+  return state.removeIn(['raw', action.data.get(action.uidField)])
+}
+
 export default {
-  start
+  start,
+  success,
+  error
 }
