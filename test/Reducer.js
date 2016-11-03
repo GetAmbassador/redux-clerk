8b3d54f5d279dc1ab1c479e64911170bcee69cfc
Corey Fields <corey@getambassador.com>
2016-11-03 18:09:18 -0400

    add stubs and new tests

--- test/Reducer.js
+++ test/Reducer.js
@@ -6,10 +6,18 @@ import { createReducer, fetchReducer, updateReducer, deleteReducer } from '../sr
 
 describe('Reducer', () => {
   let generateActionNamesStub,
+      fetchReducerStartStub,
       fetchReducerSuccessStub,
+      fetchReducerErrorStub,
       createReducerStartStub,
+      createReducerSuccessStub,
+      createReducerErrorStub,
       updateReducerStartStub,
-      deleteReducerStartStub
+      updateReducerSuccessStub,
+      updateReducerErrorStub,
+      deleteReducerStartStub,
+      deleteReducerSuccessStub,
+      deleteReducerErrorStub
 
   const config = {
     actionPrefix: 'users',
@@ -23,29 +31,53 @@ describe('Reducer', () => {
   beforeEach(() => {
     generateActionNamesStub = sinon.stub(actionNames, 'generateActionNames')
     generateActionNamesStub.onCall(0).returns({
-      start: 'USERS_CREATE'
+      start: 'USERS_CREATE',
+      success: 'USERS_CREATE_SUCCESS',
+      error: 'USERS_CREATE_ERROR'
     })
     generateActionNamesStub.onCall(1).returns({
-      success: 'USERS_FETCH_SUCCESS'
+      start: 'USERS_FETCH',
+      success: 'USERS_FETCH_SUCCESS',
+      error: 'USERS_FETCH_ERROR'
     })
     generateActionNamesStub.onCall(2).returns({
-      start: 'USERS_UPDATE'
+      start: 'USERS_UPDATE',
+      success: 'USERS_UPDATE_SUCCESS',
+      error: 'USERS_UPDATE_ERROR'
     })
     generateActionNamesStub.onCall(3).returns({
-      start: 'USERS_DELETE'
+      start: 'USERS_DELETE',
+      success: 'USERS_DELETE_SUCCESS',
+      error: 'USERS_DELETE_ERROR'
     })
+    fetchReducerStartStub = sinon.stub(fetchReducer, 'start')
     fetchReducerSuccessStub = sinon.stub(fetchReducer, 'success')
+    fetchReducerErrorStub = sinon.stub(fetchReducer, 'error')
     createReducerStartStub = sinon.stub(createReducer, 'start')
+    createReducerSuccessStub = sinon.stub(createReducer, 'success')
+    createReducerErrorStub = sinon.stub(createReducer, 'error')
     updateReducerStartStub = sinon.stub(updateReducer, 'start')
+    updateReducerSuccessStub = sinon.stub(updateReducer, 'success')
+    updateReducerErrorStub = sinon.stub(updateReducer, 'error')
     deleteReducerStartStub = sinon.stub(deleteReducer, 'start')
+    deleteReducerSuccessStub = sinon.stub(deleteReducer, 'success')
+    deleteReducerErrorStub = sinon.stub(deleteReducer, 'error')
   })
 
   afterEach(() => {
     generateActionNamesStub.restore()
+    fetchReducerStartStub.restore()
     fetchReducerSuccessStub.restore()
+    fetchReducerErrorStub.restore()
     createReducerStartStub.restore()
+    createReducerSuccessStub.restore()
+    createReducerErrorStub.restore()
     updateReducerStartStub.restore()
+    updateReducerSuccessStub.restore()
+    updateReducerErrorStub.restore()
     deleteReducerStartStub.restore()
+    deleteReducerSuccessStub.restore()
+    deleteReducerErrorStub.restore()
   })
 
   it('should throw exception if config is not provided', () => {
@@ -73,6 +105,17 @@ describe('Reducer', () => {
     expect(reducer(config)(undefined, { type: 'unknown' }).toJS()).to.deep.equal(defaultState)
   })
 
+  it('should call fetchReducer.start on USERS_FETCH', () => {
+    const action = {
+      type: 'USERS_FETCH'
+    }
+    reducer(config)(undefined, action)
+
+    expect(fetchReducerStartStub.calledOnce).to.be.true
+    expect(fetchReducerStartStub.args[0][0].toJS()).to.deep.equal(defaultState)
+    expect(fetchReducerStartStub.args[0][1]).to.deep.equal(action)
+  })
+
   it('should call fetchReducer.success on USERS_FETCH_SUCCESS', () => {
     const action = {
       type: 'USERS_FETCH_SUCCESS'
@@ -82,6 +125,17 @@ describe('Reducer', () => {
     expect(fetchReducerSuccessStub.calledOnce).to.be.true
     expect(fetchReducerSuccessStub.args[0][0].toJS()).to.deep.equal(defaultState)
     expect(fetchReducerSuccessStub.args[0][1]).to.deep.equal(action)
+  })
+
+  it('should call fetchReducer.error on USERS_FETCH_ERROR', () => {
+    const action = {
+      type: 'USERS_FETCH_ERROR'
+    }
+    reducer(config)(undefined, action)
+
+    expect(fetchReducerErrorStub.calledOnce).to.be.true
+    expect(fetchReducerErrorStub.args[0][0].toJS()).to.deep.equal(defaultState)
+    expect(fetchReducerErrorStub.args[0][1]).to.deep.equal(action)
   })
 
   it('should call createReducer.start on USERS_CREATE', () => {
