15b85c860ee2787e7d56d0dd74c9f7c9c553bddd
Ivan Sugerman <ivan@getambassador.com>
2016-12-06 16:39:49 -0500

    add tests for additionalData in delete reducer

--- test/reducers/delete.js
+++ test/reducers/delete.js
@@ -4,8 +4,10 @@ import { start, success, error } from '../../src/reducers/delete'
 
 describe('Reducers::Delete', () => {
   describe('start', () => {
-    it('should move provided uid from raw to pendingDelete', () => {
-      const previousState = Immutable.fromJS({
+    let previousState
+
+    beforeEach(() => {
+      previousState = Immutable.fromJS({
         raw: Map([[123, Immutable.fromJS({ uid: 123, test: '123' })], [234, Immutable.fromJS({ uid: 234, test: '234' })]]),
         instances: {
           test1: {
@@ -14,7 +16,9 @@ describe('Reducers::Delete', () => {
         },
         pendingDelete: Map({})
       })
+    })
 
+    it('should move provided uid from raw to pendingDelete', () => {
       const action = {
         uid: 234,
         instance: 'test1'
@@ -36,15 +40,53 @@ describe('Reducers::Delete', () => {
 
       expect(start(previousState, action).toJS()).to.deep.equal(expectedResult)
     })
+
+    it('should set additional data in state if provided', () => {
+      const action = {
+        uid: 234,
+        instance: 'test1',
+        additionalData: {
+          totalCount: 1
+        }
+      }
+
+      const expectedResult = {
+        raw: {
+          123: { uid: 123, test: '123' }
+        },
+        instances: {
+          test1: {
+            data: [123],
+            additionalData: {
+              totalCount: 1
+            }
+          }
+        },
+        pendingDelete: {
+          234: { index: 1, data: { uid: 234, test: '234' }}
+        }
+      }
+
+      expect(start(previousState, action).toJS()).to.deep.equal(expectedResult)
+    })
   })
 
   describe('success', () => {
-    it('should remove the provided item from pendingDelete', () => {
-      const previousState = Map({
+    let previousState
+
+    beforeEach(() => {
+      previousState = Immutable.fromJS({
         raw: Map([[123, Immutable.fromJS({ uid: 123, test: '123' })]]),
+        instances: {
+          test1: {
+            data: [123]
+          }
+        },
         pendingDelete: Map([[234, Immutable.fromJS({ index: 0, data: { uid: 234, test: '234' }})]])
       })
+    })
 
+    it('should remove the provided item from pendingDelete', () => {
       const action = {
         uid: 234
       }
@@ -52,6 +94,38 @@ describe('Reducers::Delete', () => {
       const expectedResult = {
         raw: {
           123: { uid: 123, test: '123' }
+        },
+        instances: {
+          test1: {
+            data: [123]
+          }
+        },
+        pendingDelete: {}
+      }
+
+      expect(success(previousState, action).toJS()).to.deep.equal(expectedResult)
+    })
+
+    it('should set additional data in state if provided', () => {
+      const action = {
+        uid: 234,
+        instance: 'test1',
+        additionalData: {
+          totalCount: 1
+        }
+      }
+
+      const expectedResult = {
+        raw: {
+          123: { uid: 123, test: '123' }
+        },
+        instances: {
+          test1: {
+            data: [123],
+            additionalData: {
+              totalCount: 1
+            }
+          }
         },
         pendingDelete: {}
       }
@@ -61,8 +135,10 @@ describe('Reducers::Delete', () => {
   })
 
   describe('error', () => {
-    it('should re-add the deleted item', () => {
-      const previousState = Immutable.fromJS({
+    let previousState
+
+    beforeEach(() => {
+      previousState = Immutable.fromJS({
         raw: Map([[123, Immutable.fromJS({ uid: 123, test: '123' })]]),
         instances: {
           test1: {
@@ -71,7 +147,9 @@ describe('Reducers::Delete', () => {
         },
         pendingDelete: Map([[234, Immutable.fromJS({ index: 1, data: { uid: 234, test: '234' }})]])
       })
+    })
 
+    it('should re-add the deleted item', () => {
       const action = {
         uid: 234,
         instance: 'test1'
@@ -85,6 +163,34 @@ describe('Reducers::Delete', () => {
         instances: {
           test1: {
             data: [123,234]
+          }
+        },
+        pendingDelete: {}
+      }
+
+      expect(error(previousState, action).toJS()).to.deep.equal(expectedResult)
+    })
+
+    it('should set additional data in state if provided', () => {
+      const action = {
+        uid: 234,
+        instance: 'test1',
+        additionalData: {
+          totalCount: 2
+        }
+      }
+
+      const expectedResult = {
+        raw: {
+          123: { uid: 123, test: '123' },
+          234: { uid: 234, test: '234' }
+        },
+        instances: {
+          test1: {
+            data: [123,234],
+            additionalData: {
+              totalCount: 2
+            }
           }
         },
         pendingDelete: {}
